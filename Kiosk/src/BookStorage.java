c70e04a00f44f6c851557c07c4be9bb27c46db36
Sander J. Skarmyr <Sander@skarmyr.no>
2018-03-12 14:13:51 +0100

    Refactoring of book adding
    Refactored the method of adding new books to the book storage

--- Kiosk/src/BookStorage.java
+++ Kiosk/src/BookStorage.java
@@ -1,96 +1,83 @@
 import java.util.ArrayList;
+
 /**
- * The list that holds the different books, papers and newspapers 
+ * The list that holds the different books, papers and newspapers
  * that is in the kiosks stock.
  *
  * @author Jan Anton, Sander Joachim, Karl Kristian
  * @version 0.2
  */
-public class BookStorage
-{
+public class BookStorage {
     private ArrayList<Book> listOfBooks;
+    private Book book;
 
     /**
      * Initiates the list of all the books.
-     * 
      */
-    public BookStorage()
-    {
+    public BookStorage() {
         this.listOfBooks = new ArrayList<Book>();
     }
 
     /**
-     * Add an already excisting book to the list.
-     * 
-     * @param book is the name of the object that hold the book.
+     * Creates a new, empty book and fills it.
      */
-    public void addBook(Book book)
-    {
-        this.listOfBooks.add(book);
+    public void addNewBook(String author, String title, String publisher, String edition, String genre,
+                           String releaseDate, int pages, int price, boolean series, String seriesName) {
+        this.book = new Book();
+        this.book.setTitle(title);
+        this.book.setAuthor(author);
+        this.book.setPublisher(publisher);
+        this.book.setEdition(edition);
+        this.book.setGenre(genre);
+        this.book.setReleaseDate(releaseDate);
+        this.book.setPages(pages);
+        this.book.setPrice(price);
+        this.book.setSeries(series);
+        if (series) {
+            this.book.setSeriesName(seriesName);
+        }
+        addBook();
     }
-    
+
     /**
-     * Adds a new book object and adds it to the list.
-     * 
-     * @param bookAuthor author of the book.
-     * @param bookTitle title of the book.
-     * @param bookPublisher the publisher of the book.
-     * @param bookEdition the edition of the book.
-     * @param bookPages total pages in the book.
-     * @param bookPrice the price of the book in NOK.
-     * @param bookGenre the genre of the book.
-     * @param bookReleaseDate the release date of the book.
-     * @param isBookASeries denotes if the book is part of a series or not.
+     * The currently selected book to the list.
      */
-    public void addNewBook(String bookAuthor, String bookTitle, String bookPublisher, 
-    String bookEdition, String bookGenre, String bookReleaseDate, 
-    int bookPages, int bookPrice, boolean isBookASeries)
-    {
-        listOfBooks.add( new Book(bookAuthor, bookTitle, bookPublisher, 
-            bookEdition, bookGenre, bookReleaseDate, 
-            bookPages, bookPrice, isBookASeries));
+    public void addBook() {
+        this.listOfBooks.add(this.book);
     }
-    
+
     /**
      * Removes a book from the list based on its position in the list.
-     * 
+     *
      * @param index is the position in the list.
      */
-    public void removeBookFromList(int index)
-    {
+    public void removeBookFromList(int index) {
         listOfBooks.remove(index);
     }
-       
+
     /**
      * Prints a book with the same author name as the input.
-     * 
+     *
      * @param author is the name of the author.
      */
-    public void getBookByAuthor(String author)
-    {
+    public void getBookByAuthor(String author) {
         Book foundBook = null;
-        for (Book b : this.listOfBooks)
-        {
-            if (b.getAuthor().equals(author))
-            {
+        for (Book b : this.listOfBooks) {
+            if (b.getAuthor().equals(author)) {
                 b.printDetailsAsString();
             }
         }
     }
-    
+
     /**
      * Prints a book with the same publisher as the input.
-     * 
+     *
      * @param publisher is the name of the publisher.
-     * 
      */
-    public void getBookByPublisher(String publisher)
-    {
+    public void getBookByPublisher(String publisher) {
         Book foundBook = null;
-        for (Book b : this.listOfBooks)
-        {
-            if (b.getPublisher().equals(publisher))
-            {
+        for (Book b : this.listOfBooks) {
+            if (b.getPublisher().equals(publisher)) {
                 b.printDetailsAsString();
             }
         }
@@ -98,17 +85,14 @@ public class BookStorage
 
     /**
      * Prints a book with the same title and publisher as the input.
-     * 
-     * @param title is the name of the title.
+     *
+     * @param title     is the name of the title.
      * @param publisher is the name of the publisher.
      */
-    public void getBookByTitleAndPublisher(String title, String publisher)
-    {
+    public void getBookByTitleAndPublisher(String title, String publisher) {
         Book foundBook = null;
-        for (Book b : this.listOfBooks)
-        {
-            if ((b.getTitle().equals(title)) && (b.getPublisher().equals(publisher)))
-            {
+        for (Book b : this.listOfBooks) {
+            if ((b.getTitle().equals(title)) && (b.getPublisher().equals(publisher))) {
                 b.printDetailsAsString();
             }
         }
@@ -117,11 +101,9 @@ public class BookStorage
     /**
      * Prints all the books stored.
      */
-    public void listAllBooks()
-    {
+    public void listAllBooks() {
         Book foundBook = null;
-        for (Book b : this.listOfBooks)
-        {
+        for (Book b : this.listOfBooks) {
             b.printDetailsAsString();
         }
     }
