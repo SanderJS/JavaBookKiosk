4066806697590b335c4db022f859a8fd5c9c6cf1
Mordi490 <jananton007@hotmail.com>
2018-03-12 12:04:41 +0100

    test
    test

--- Kiosk/ApplicationUI.java
+++ Kiosk/ApplicationUI.java
@@ -1,3 +1,75 @@
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+/**
+ * Makes up the user interface (text based) of the application.
+ * Responsible for all user interaction, like displaying the menu
+ * and receiving input from the user.
+ * 
+ * @author asty
+ * @version 1.0
+ */
+public class ApplicationUI 
+{
+
+   
+    // The menu tha will be displayed. Please edit/alter the menu
+    // to fit your application (i.e. replace "prodct" with "litterature"
+    // etc.
+    private String[] menuItems = {
+        "1. List all products",
+        "2. Add new product",
+        "3. Find a product by name",
+    };
+
+    /**
+     * Creates an instance of the ApplicationUI User interface. 
+     */
+    public ApplicationUI() 
+    {
+    }
+
+    /**
+     * Starts the application by showing the menu and retrieving input from the
+     * user.
+     */
+    public void start() 
+    {
+        this.init();
+
+        boolean quit = false;
+
+        while (!quit) 
+        {
+            try 
+            {
+                int menuSelection = this.showMenu();
+                switch (menuSelection) 
+                {
+                    case 1:
+                        this.listAllProducts();
+                        break;
+
+                    case 2:
+                        this.addNewProduct();
+                        break;
+
+                    case 3:
+                        this.findProductByName();
+                        break;
+
+                    case 4:
+                        System.out.println("\nThank you for using Application v0.1. Bye!\n");
+                        quit = true;
+                        break;
+
+                    default:
+                }
+            } 
+            catch (InputMismatchException ime) 
+            {
+                System.out.println("\nERROR: Please provide a number between 1 and " + this.menuItems.length + "..\n");
+            }
+        }        
+        
+    }
