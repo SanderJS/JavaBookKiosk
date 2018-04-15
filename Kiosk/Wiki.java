97b702424cb67ce36d0c836714f8a13489fd7850
Mordi490 <jananton007@hotmail.com>
2018-04-16 01:08:48 +0200

    forslag til wiki type

--- Kiosk/Wiki.java
+++ Kiosk/Wiki.java
@@ -1,2 +1,26 @@
 import java.util.ArrayList;
 
+/**
+ * this is a wiki page for a project
+ * 
+ * @author Jan Anton and Joachim
+ * @version 1.0
+ */
+public Class Wiki extends Text {
+    private ArrayList<String> contributers;
+    private String project;
+
+    /**
+     * constructor for class Wiki
+     * 
+     * @param the project's name
+     */
+    public Wiki(String project) {
+        this.project = project;
+    }
+
+    public String getProjectName()
+    {
+        return project;
+    }
+}
