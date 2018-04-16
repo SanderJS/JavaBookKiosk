64c21196b182e84982a54636885ee036d5b71c3a
Mordi490 <jananton007@hotmail.com>
2018-04-16 08:53:07 +0200

    added javadoc

--- Kiosk/Wiki.java
+++ Kiosk/Wiki.java
@@ -1,13 +1,16 @@
 import java.util.ArrayList;
 
 /**
- * this is a wiki page for a project
+ * this is a dummy wiki page for a fake project(s)
+ * it contains a list of contributers, a list of 
+ * related projects and the name of the main project.
  * 
  * @author Jan Anton and Joachim
  * @version 1.0
  */
 public Class Wiki extends Text {
     private ArrayList<String> contributers;
+    private ArrayList<String> projects;
     private String project;
 
     /**
@@ -19,8 +22,25 @@ public Class Wiki extends Text {
         this.project = project;
     }
 
-    public String getProjectName()
+    /**
+     * gets the name of the main project
+     * 
+     * @return the main project's name
+     */
+    public String getMainProjectName()
     {
         return project;
     }
+
+    /**
+     * adds a related project to the main project
+     * 
+     * @projectName the name of the related project
+     */
+    public void addPorjects(String projectName)
+    {
+        projects.add(ProjectName);
+    }
+
+    public void getPorject(String name)
 }
--- Kiosk/magazine.java
+++ Kiosk/magazine.java
@@ -1,5 +1,14 @@
 import java.util.ArrayList;
 
+/**
+ * this is a magazine, a extension of text (based media)
+ * a magazine contains a lot of pictures
+ * a larger pool of authors than ordinary books
+ * articles about different subjects.
+ * 
+ * @author Jan Anton, Joachim
+ * @version 1.0
+ */
 public class Magazine extends Text {
     prviate ArrayList<String> pictures;
     private int ArrayList<String> articles;
@@ -8,6 +17,10 @@ public class Magazine extends Text {
     /**
      * Constructor for objects of subclass Magazine
      * 
+     * creates and instance of a text with super
+     * creates a new list for pictures
+     * creates a new list of articles
+     * creates a new list for authors
      * 
      */
     public Magazine()
@@ -18,16 +31,31 @@ public class Magazine extends Text {
         this.authors = new ArrayList<>();
     }
 
+    /**
+     * adds a picture to the magazine
+     * 
+     * @param filename the name of the picture file
+     */
     public void addPicture(String filename)
     {
         pictures.add(filename);
     }
 
+    /**
+     * adds a article to the magazine
+     * 
+     * @param title the title of the article
+     */
     public void addArticle(String title)
     {
         articles.add(title);
     }
 
+    /**
+     * adds a author to the magazine 
+     * 
+     * @param author the author of the article
+     */
     public void addAuthor(String author)
     {
         authors.add(author);
